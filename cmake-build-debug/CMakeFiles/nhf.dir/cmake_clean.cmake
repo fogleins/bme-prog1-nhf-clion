683a02d1c84f3d2dccaddffb0e15638e40efe897
44982432 <fogleins@hotmail.hu>
2019-11-07 00:34:33 +0100

    add

---
+++ cmake-build-debug/CMakeFiles/nhf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nhf.dir/main.c.obj"
+  "libnhf.dll.a"
+  "nhf.exe"
+  "nhf.exe.manifest"
+  "nhf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nhf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
