2247c8ab700d248c3a972b5e41eb978447a3e1c1
44982432 <fogleins@hotmail.hu>
2019-11-08 21:12:03 +0100

    SDL parancsok kiszervezése mainből függvényekbe (board.c)

--- board.c
+++ board.c
@@ -2,7 +2,53 @@
 // Created by Simon on 2019-11-07.
 //
 #include <stdlib.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 
+/** kockadobást szimulál **/
 int dice(void) {
     return rand() % 7;
+}
+
+/** létrehoz egy ablakot és egy renderert**/
+//TODO: pointereket adjon vissza (window és rederer)
+void ablak_letrehozasa(int szelesseg, int magassag) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        SDL_Log("Nem indithato az SDL: %s", SDL_GetError());
+        exit(1);
+    }
+    SDL_Window *window = SDL_CreateWindow("Legyel olimpiai bajnok!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, szelesseg, magassag, 0);
+    if (window == NULL) {
+        SDL_Log("Az ablak letrehozasa nem sikerult: %s.", SDL_GetError());
+        exit(1);
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    if (renderer == NULL) {
+        SDL_Log("Nem hozhato letre a megjelenito: %s", SDL_GetError());
+        exit(1);
+    }
+    SDL_RenderClear(renderer);
+}
+
+/** kiír egyelőre csak hardcodeolva van**/
+//TODO: renderer legyen definiálva
+void ablakra_kiir(void) {
+    // A sztring kiírása SDL-lel
+    //felesleges, mert mindig csak egy jegyű szám lehet, így biztosan 9 karakternyi hely kell
+    //char* kiiras = (char*) malloc(sizeof(char) * 9); /* string: Kocka: [int]\0 => 9 chars */
+    char kiiras[9] = "Kocka: ";
+    kiiras[7] = (char) dice();
+    kiiras[8] = '\0';
+    stringRGBA(renderer, 300, 280, kiiras, 255, 255, 255, 255);
+    SDL_RenderPresent(renderer);
+}
+
+void ablak_bezarasa(void) {
+    /* varunk a kilepesre */
+    SDL_Event ev;
+    while (SDL_WaitEvent(&ev) && ev.type != SDL_QUIT) {}
+
+    /* ablak bezarasa */
+    SDL_Quit();
 }
--- main.c
+++ main.c
@@ -3,43 +3,14 @@
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>
 #include <string.h>
 #include "board.c"
 
 int main(int argc, char *argv[]) {
     srand(time(0));
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        SDL_Log("Nem indithato az SDL: %s", SDL_GetError());
-        exit(1);
-    }
-    SDL_Window *window = SDL_CreateWindow("Legyel olimpiai bajnok!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 576, 0);
-    if (window == NULL) {
-        SDL_Log("Az ablak letrehozasa nem sikerult: %s.", SDL_GetError());
-        exit(1);
-    }
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-    if (renderer == NULL) {
-        SDL_Log("Nem hozhato letre a megjelenito: %s", SDL_GetError());
-        exit(1);
-    }
-    SDL_RenderClear(renderer);
-
-    // A sztring kiírása SDL-lel
-    //felesleges, mert mindig csak egy jegyű szám lehet, így biztosan 9 karakternyi hely kell
-    //char* kiiras = (char*) malloc(sizeof(char) * 9); /* string: Kocka: [int]\0 => 9 chars */
-    char kiiras[9] = "Kocka: ";
-    kiiras[7] = (char) dice();
-    kiiras[8] = '\0';
-    stringRGBA(renderer, 300, 280, kiiras, 255, 255, 255, 255);
-    SDL_RenderPresent(renderer);
-
-    /* varunk a kilepesre */
-    SDL_Event ev;
-    while (SDL_WaitEvent(&ev) && ev.type != SDL_QUIT) {}
-
-    /* ablak bezarasa */
-    SDL_Quit();
+    ablak_letrehozasa(1024, 576);
+    ablakra_kiir();
+    ablak_bezarasa();
 
     return 0;
 }
